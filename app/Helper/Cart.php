b5083348d15715a0ba1db921520b7bb9abf604ae
İskender <100355107+iskenderErgul@users.noreply.github.com>
2024-03-19 00:37:44 +0300

    .

--- app/Helper/Cart.php
+++ app/Helper/Cart.php
@@ -12,65 +12,86 @@ class Cart
 {
     public static function getCount(): int
     {
-        if ($user = auth()->user()) {
-            return CartItem::whereUserId($user->id)->count(); //sum('quantity')
+        if ($user = auth()->user()) { // kullanıcı kimliği alınır. Eğer giriş yapmışsa
+            return CartItem::whereUserId($user->id)->count();  // veritabanında  bu kullanıcının sepetindeki toplam ürün sayısını sayar
         } else {
             return array_reduce(self::getCookieCartItems(), fn ($carry) => $carry + 1, 0);
+            //array_reduce fonksiyonu, bir dizi üzerinde bir işlevi uygular ve sonuç olarak tek bir değer döndürür. Bu fonksiyon üç parametre alır:
+            //
+            //Dizi: İşlemin uygulanacağı dizi.
+            //Gerçekleştirilecek işlev: Her bir dizi öğesi üzerinde çalışacak işlev.
+            //Başlangıç değeri: İşleme başlamadan önce kullanılacak bir başlangıç değeri.
+
         }
     }
 
     public static function getCartItems()
     {
-        if ($user = auth()->user()) {
+        if ($user = auth()->user()) {// kullanıcı kimliği alınır. Eğer giriş yapmışsa
             return CartItem::whereUserId($user->id)->get()->map(fn (CartItem $item) => ['product_id' => $item->product_id, 'quantity' => $item->quantity]);
+            // veritabanında (CartItem modeli kullanılarak) kullanıcının sepetinde bulunan her bir öğenin ürün kimliği (product_id) ve miktarını (quantity) alır.
+            // Bu bilgiler, bir dizi olarak dönüştürülür ve döndürülür
         } else {
-            return self::getCookieCartItems();
+            return self::getCookieCartItems(); //Eğer kullanıcı giriş yapmamışsa, yani bir misafir kullanıcıysa, sepet bilgileri çerezler aracılığıyla alınır
         }
     }
 
     public static function getCookieCartItems()
     {
         return json_decode(request()->cookie('cart_items', '[]'), true);
+        // Bu, HTTP isteği üzerinden 'cart_items' adlı çerezin değerini alır.
+        // Eğer çerez bulunamazsa, varsayılan olarak boş bir dizi ('[]') döndürülür.
+        // çerezden alınan değeri JSON formatından PHP dizisine dönüştürür.
+
     }
 
-    public static function setCookieCartItems(array $cartItems)
+    public static function setCookieCartItems(array $cartItems): void
     {
         Cookie::queue('cart_items', json_encode($cartItems), 60*24*30);
+        // Bu, çerezde 'cart_items' adıyla verilen sepet öğelerini saklar.
+        // Burada 602430 ifadesi, çerezin bir ay (30 gün) boyunca geçerli olacağını belirtir.
+        // Çerez, tarayıcı tarafından saklanır ve bu süre sonunda otomatik olarak silinir.
     }
 
-    public static function saveCookieCartItems()
+
+    // Kullanıcının sepetinde veritabanında bulunan öğelerle, çerezlerdeki sepet öğelerini karşılaştırı
+    public static function saveCookieCartItems(): void
     {
-        $user = auth()->user();
+        $user = auth()->user(); //oturum açmış kullanıcı bilgileri alınır
         $userCartItems = CartItem::where(['user_id' => $user->id])->get()->keyBy('product_id');
+        // veritabanından kullanıcıya ait olan sepet bilgilerini alıyoruz
+        // bunu ürünün id sine göre diziyoruz
+
         $savedCartItems = [];
-        foreach (self::getCookieCartItems() as $cartItem) {
-            if (isset($userCartItems[$cartItem['product_id']])) {
-                $userCartItems[$cartItem['product_id']]->update(['quantity' => $cartItem['quantity']]);
+        foreach (self::getCookieCartItems() as $cartItem) { // çerezlerden kullanıcının sepetini alıyoruz ve bunu bir döngüye sokuyoruz.Buradaki amaç aynı ürün olup olmadıgını kontrol etmektir.
+            if (isset($userCartItems[$cartItem['product_id']])) { //Eğer kullanıcının sepetinde aynı ürün varsa
+                $userCartItems[$cartItem['product_id']]->update(['quantity' => $cartItem['quantity']]); //Miktar güncellenir
                 continue;
             }
-            $savedCartItems[] = [
+            // eğer kullanıcının sepetinde bu ürün yoksa
+            $savedCartItems[] = [ // bu ürünü eklemek için bir diziye atıyoruz
                 'user_id' => $user->id,
                 'product_id' => $cartItem['product_id'],
                 'quantity' => $cartItem['quantity'],
             ];
         }
-        if (!empty($savedCartItems)) {
-            CartItem::insert($savedCartItems);
+        if (!empty($savedCartItems)) { //eğer dizi boş değilse
+            CartItem::insert($savedCartItems); // veritabanına olmayan ürün ekleniyor
         }
     }
 
-    public static function moveCartItemsIntoDb()
+    public static function moveCartItemsIntoDb(): void
     {
         $request = request();
         $cartItems = self::getCookieCartItems();
         $newCartItems = [];
         foreach ($cartItems as $cartItem) {
-            // Check if the record already exists in the database
+            // Veritabanında aynı ürünün ('product_id') ve kullanıcının ('user_id') sepet öğesi olup olmadığı kontrol edilir.
             $existingCartItem = CartItem::where([
                 'user_id' => $request->user()->id,
                 'product_id' => $cartItem['product_id'],
             ])->first();
-
+            // Eğer zaten bir sepet öğesi bulunuyorsa, bu öğe zaten veritabanında olduğu için $newCartItems dizisine eklenmez ve işlem devam eder.
             if (!$existingCartItem) {
                 // Only insert if it doesn't already exist
                 $newCartItems[] = [
@@ -81,8 +102,7 @@ class Cart
             }
         }
 
-
-        if (!empty($newCartItems)) {
+        if (!empty($newCartItems)) { // ğer $newCartItems dizisi boş değilse, yani çerezlerde veritabanında olmayan yeni sepet öğeleri bulunuyorsa, bu öğeler veritabanına eklenir
             // Insert the new cart items into the database
             CartItem::insert($newCartItems);
         }
@@ -93,9 +113,9 @@ class Cart
     {
         $cartItems = self::getCartItems();
 
-        $ids = Arr::pluck($cartItems, 'product_id');
-        $products = Product::whereIn('id', $ids)->with('product_images')->get();
-        $cartItems = Arr::keyBy($cartItems, 'product_id');
-        return [$products, $cartItems];
+        $ids = Arr::pluck($cartItems, 'product_id'); // Bu, sepet öğelerinin ürün kimliklerini (product_id) içeren bir dizi oluşturur
+        $products = Product::whereIn('id', $ids)->with('product_images')->get(); // Bu, ürünler tablosunda, daha önce belirlenen ürün kimlikleri listesinde ($ids dizisi) bulunan ürünleri alır
+        $cartItems = Arr::keyBy($cartItems, 'product_id'); // Bu, sepet öğelerini bir dizi olarak alır ve ürün kimliğine (product_id) göre dizi elemanlarını indeksler.
+        return [$products, $cartItems]; // Son olarak, bu metod, hem ürünlerin hem de sepet öğelerinin birleştirilmiş olarak döndürülmesi gerektiğinden, bu iki bilgiyi bir dizi olarak döndürür
     }
 }
---
+++ app/Http/Controllers/User/CartController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Helper\Cart;
+use App\Http\Controllers\Controller;
+use App\Models\CartItem;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function view() {
+
+    }
+    public function store(Request $request, Product $product)
+    {
+        // kullanıcıdan ürün miktarını alınır. eğer ürün miktarı gelmesse varsayılan olarak 1 atarır.
+        $quantity = $request->post('quantity',1);
+
+        //kullanıcının giriş yapıp yapmadığı kontrol edilir
+        $user = $request->user();
+
+
+        if($user) {//Eğer kullanıcı giriş yapmışsa,
+
+            //  o zaman veritabanında(CartItem modeli kullanılarak) bu kullanıcının sepetinde bu ürünün olup olmadığını kontrol eder
+            $cartItem = CartItem::where(['user_id' => $user->id , 'product_id' => $product->id])->first();
+
+            if ($cartItem) {  //Eğer ürün zaten sepette varsa, miktarını artırır
+                $cartItem->increment('quantity');
+            }else {  //Eğer ürün sepette yoksa, yeni bir kart öğesi oluşturur
+                CartItem:: create([
+                    'user_id'=> $user->id,
+                    'product_id' => $product->id,
+                    'quantity' => 1,
+                ]);
+            }
+
+        }else { //Eğer kullanıcı giriş yapmamışsa
+
+            $cartItems = Cart::getCookieCartItems(); //Sepet bilgilerini cookiler ile alırız
+            $isProductExists = false ;
+            foreach ($cartItems  as $item) {
+                if ($item['product_id']===$product->id)  // Daha sonra bu ürünün eklenip eklenmediği kontrol edilir
+                {
+                    $item['quantity'] += $quantity; // eğer eklenmişşe ürünün miktarı arttırılır
+                    $isProductExists = true;
+                    break;
+
+                }
+            }
+
+            if (!$isProductExists) //  Eğer eklenmemişse, yeni bir sepet öğesi oluşturur
+            {
+                $cartItems[] =[
+                    'user_id' => null,
+                    'product_id' => $product->id,
+                    'quantity' => $quantity,
+                    'price' => $product->price,
+                    ];
+            }
+            Cart::setCookieCartItems($cartItems); //$cartItems değişkeninde bulunanları çereze kaydediyor
+        }
+
+        return redirect()->back()->with('success','cart added successfully');
+
+
+
+    }
+    public  function update(Request $request, Product $product) {
+
+        $quantity = $request->integer('quantity'); //miktar bilgisini aldık
+        $user = $request->user();
+        if ($user) { // eğer kullanıcı varsa
+            //  o zaman veritabanında (CartItem modeli kullanılarak) bu kullanıcının sepetindeki belirli bir ürünün miktarını  gelen miktar ile günceller
+            CartItem::where(['user_id' => $user->id, 'product_id' => $product->id])->update(['quantity' => $quantity]);
+        } else {  //  eğer kullanıcı giriş yapmamışsa
+            $cartItems = Cart::getCookieCartItems();  //  sepet bilgileri çerezler aracılığıyla alınır
+            foreach ($cartItems as &$item) {  //  Eğer ürün sepete daha önce eklenmişse, ilgili ürünün miktarını günceller
+                if ($item['product_id'] === $product->id) {
+                    $item['quantity'] = $quantity;
+                    break;
+                }
+            }
+            Cart::setCookieCartItems($cartItems);   //  Güncellenmiş sepet bilgileri çerezlere kaydedilir
+        }
+
+        return redirect()->back();
+
+    }
+    public function  delete(Request $request, Product $product) {
+        $user = $request->user();
+        if ($user) {
+            CartItem::query()->where(['user_id' => $user->id, 'product_id' => $product->id])->first()?->delete();
+            if (CartItem::count() <= 0) {
+                return redirect()->route('home')->with('info', 'your cart is empty');
+            } else {
+                return redirect()->back()->with('success', 'item removed successfully');
+            }
+        } else {
+            $cartItems = Cart::getCookieCartItems();
+            foreach ($cartItems as $i => &$item) {
+                if ($item['product_id'] === $product->id) {
+                    array_splice($cartItems, $i, 1);
+                    break;
+                }
+            }
+            Cart::setCookieCartItems($cartItems);
+            if (count($cartItems) <= 0) {
+                return redirect()->route('home')->with('info', 'your cart is empty');
+            } else {
+                return redirect()->back()->with('success', 'item removed successfully');
+            }
+        }
+    }
+}
